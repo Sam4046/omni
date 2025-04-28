77ad8674cdab7baa60dbf099040fc8d9ea18a6ab
Sam4046 <sami.kreish@gmail.com>
2025-04-28 23:01:23 +0200

    lcd_class

--- main.py
+++ main.py
@@ -1,21 +1,7 @@
-import RPi.GPIO as gp
-from gpiozero import OutputDevice
-import csv
-import os  # Für Datei-Existenzprüfung
-from time import sleep, time
-import sys
-import termios
-import tty
 from classes.key import *
 from classes.mojo import ParkhausSystem as pk
 import classes.lcd as screen 
 
-# gp.setmode(gp.BOARD) # Board Pins (deaktiviert)
-gp.setmode(gp.BCM) # Mode in pi nach GPIO Pins numm
-
-# Sensore definieren
-gp.setup(24, gp.IN, pull_up_down=gp.PUD_UP) 
-gp.setup(25, gp.IN, pull_up_down=gp.PUD_UP)
 
 # die freie verfügbare Parkplätze 
 FreeP = pk.get_parkp()
